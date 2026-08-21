c1a50c504de0f3a6139642a40f95d1ee0e3ed29e
daisubaiacc-maker <dai.subai.acc@gmail.com>
2026-08-21 10:32:44 +0900

    Add in-memory project store boundary

---
+++ src/core/ProjectStore.ts
@@ -0,0 +1,17 @@
+import { createProject, type ProjectData } from './Project';
+
+export class ProjectStore {
+  private project: ProjectData = createProject();
+
+  get(): ProjectData {
+    return this.project;
+  }
+
+  replace(project: ProjectData): void {
+    this.project = project;
+  }
+
+  update(mutator: (project: ProjectData) => void): void {
+    mutator(this.project);
+  }
+}
